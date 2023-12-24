f77685e23998dd650bd74a519e0206e4b4979584
aderemilekun <adelefatai@yahoo.com>
2023-12-24 18:48:38 +0100

    Core data with Swift UI

---
+++ Devote/Utility/Constant.swift
@@ -0,0 +1,21 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Aderemilekun Adele on 24/12/2023.
+//
+
+import SwiftUI
+
+// MARK: - FORMATTER
+
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UX
