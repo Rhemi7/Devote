9a5165c56221176ae2a248cc9ef4b34b04944e30
aderemilekun <adelefatai@yahoo.com>
2023-12-25 19:22:27 +0100

    Adding items to storage

--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
     
     // MARK: - PROPERTY
     
+    @State var task: String = ""
+    
     // FETCHING DATA
     @Environment(\.managedObjectContext) private var viewContext
 
@@ -26,6 +28,9 @@ struct ContentView: View {
         withAnimation {
             let newItem = Item(context: viewContext)
             newItem.timestamp = Date()
+            newItem.task = task
+            newItem.completion = false
+            newItem.id = UUID()
 
             do {
                 try viewContext.save()
@@ -55,26 +60,61 @@ struct ContentView: View {
 
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+            VStack {
+                VStack(spacing: 16) {
+                    TextField("New Task", text: $task)
+                        .padding()
+                        .background(
+                            Color(UIColor.systemGray6)
+                        )
+                        .cornerRadius(10)
+                    
+                    Button(action: {
+                        addItem()
+                    }, label: {
+                        Spacer()
+                        Text("SAVE")
+                        Spacer()
+                    })
+                    .padding()
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .background(Color.pink)
+                    .cornerRadius(10)
+                } //: VSTACK
+                .padding()
+                
+                List {
+                    ForEach(items) { item in
+                    
+                            VStack(alignment: .leading) {
+                                Text(item.task ?? "")
+                                    .font(.headline)
+                                    .fontWeight(.bold)
+                                
+                                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                                    .font(.footnote)
+                                    .foregroundColor(.gray)
+                            } //: LIST ITEM
+                      
                     }
-                }
-                .onDelete(perform: deleteItems)
-            } //: LIST
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    EditButton()
-                }
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+                    .onDelete(perform: deleteItems)
+                } //: LIST
+                .navigationBarTitle("Daily Tasks", displayMode: .large)
+                .toolbar {
+                    #if os(iOS)
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        EditButton()
                     }
-                }
+                    
+                    #endif
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button(action: addItem) {
+                            Label("Add Item", systemImage: "plus")
+                        }
+                    }
             } //: TOOLBAR
+            } //: VSTACK
             Text("Select an item")
         } //: NAVIGATION
     }
