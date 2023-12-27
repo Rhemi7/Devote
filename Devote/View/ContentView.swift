ec91fb617af2806c4fdae93618d6c49c4bffe4ae
aderemilekun <adelefatai@yahoo.com>
2023-12-27 15:20:04 +0100

    Implemented New task view

--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -14,9 +14,7 @@ struct ContentView: View {
     
     @State var task: String = ""
     
-    private var isButtonDisabled: Bool {
-        task.isEmpty
-    }
+   
     
     // FETCHING DATA
     @Environment(\.managedObjectContext) private var viewContext
@@ -28,26 +26,7 @@ struct ContentView: View {
     
     // MARK: - FUNCTION
     
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-            newItem.task = task
-            newItem.completion = false
-            newItem.id = UUID()
-
-            do {
-                try viewContext.save()
-            } catch {
-            
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-            
-            task = ""
-            hideKeyboard()
-        }
-    }
+  
 
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
@@ -71,29 +50,7 @@ struct ContentView: View {
         NavigationView {
             ZStack {
                 VStack {
-                    VStack(spacing: 16) {
-                        TextField("New Task", text: $task)
-                            .padding()
-                            .background(
-                                Color(UIColor.systemGray6)
-                            )
-                            .cornerRadius(10)
-                        
-                        Button(action: {
-                            addItem()
-                        }, label: {
-                            Spacer()
-                            Text("SAVE")
-                            Spacer()
-                        })
-                        .disabled(isButtonDisabled)
-                        .padding()
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .background(isButtonDisabled ? Color.gray : Color.pink)
-                        .cornerRadius(10)
-                    } //: VSTACK
-                    .padding()
+                    
                     
                     List {
                         ForEach(items) { item in
---
+++ Devote/View/NewTaskItemView.swift
@@ -0,0 +1,89 @@
+//
+//  NewTaskItemView.swift
+//  Devote
+//
+//  Created by Aderemilekun Adele on 27/12/2023.
+//
+
+import SwiftUI
+
+struct NewTaskItemView: View {
+    // MARK: - PROPERTIES
+    
+    @Environment(\.managedObjectContext) private var viewContext
+    @State private var task: String = ""
+    
+    private var isButtonDisabled: Bool {
+        task.isEmpty
+    }
+    
+    // MARK: - function
+    private func addItem() {
+        withAnimation {
+            let newItem = Item(context: viewContext)
+            newItem.timestamp = Date()
+            newItem.task = task
+            newItem.completion = false
+            newItem.id = UUID()
+
+            do {
+                try viewContext.save()
+            } catch {
+            
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+            
+            task = ""
+            hideKeyboard()
+        }
+    }
+    // MARK: - BODY
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            VStack(spacing: 16) {
+                TextField("New Task", text: $task)
+                    .foregroundColor(.pink)
+                    .font(.system(size: 24, weight: .bold, design: .rounded))
+                    .padding()
+                    .background(
+                        Color(UIColor.systemGray6)
+                    )
+                    .cornerRadius(10)
+                
+                Button(action: {
+                    addItem()
+                }, label: {
+                    Spacer()
+                    Text("SAVE")
+                        .font(.system(size: 24, weight: .bold, design: .rounded))
+                    Spacer()
+                })
+                .disabled(isButtonDisabled)
+                .padding()
+                .foregroundColor(.white)
+                .background(isButtonDisabled ? Color.blue : Color.pink)
+                .cornerRadius(10)
+            } //: VSTACK
+            .padding(.horizontal)
+            .padding(.vertical, 20)
+            .background(
+                Color.white
+            )
+            .cornerRadius(16)
+            .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.65), radius: 24)
+            .frame(maxWidth: 640)
+        } //: VSTACK
+        .padding()
+    }
+}
+
+struct NewTaskItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewTaskItemView()
+            .background(Color.gray.edgesIgnoringSafeArea(.all))
+    }
+}
