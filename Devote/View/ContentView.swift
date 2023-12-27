11f101fe8735a36f7c1d137c5d49b9da078ea279
aderemilekun <adelefatai@yahoo.com>
2023-12-27 15:29:34 +0100

    New Item Button and click to show New task item View

--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
     // MARK: - PROPERTY
     
     @State var task: String = ""
-    
+    @State private var showNewTaskItem: Bool = false
    
     
     // FETCHING DATA
@@ -49,9 +49,30 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             ZStack {
+                // MARK: - MAIN VIEW
                 VStack {
                     
+                    // MARK: - HEADER
                     
+                    Spacer(minLength: 80)
+                    // MARK: - NEW TASK BUTTON
+                    
+                    Button(action: {
+                        showNewTaskItem = true
+                    }, label: {
+                        Image(systemName: "plus.circle")
+                            .font(.system(size: 30, weight: .semibold, design: .rounded))
+                        Text("New Task")
+                            .font(.system(size: 24, weight: .bold, design: .rounded))
+                    })
+                    .foregroundColor(.white)
+                    .padding(.horizontal,20)
+                    .padding(.vertical, 15)
+                    .background(
+                        LinearGradient(gradient: Gradient(colors: [Color.pink, Color.blue]), startPoint: .leading, endPoint: .trailing).clipShape(Capsule()))
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.25), radius: 8, x: 0.0, y: 4.0)
+                    
+                    // MARK: - TASKs
                     List {
                         ForEach(items) { item in
                         
@@ -71,6 +92,12 @@ struct ContentView: View {
                     .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.3), radius: 12)
                     .padding(.vertical, 0)
                     .frame(maxWidth: 640)
+                    
+                    // MARK: - NEW TASK ITEM VIEW
+                    
+                    if showNewTaskItem {
+                        NewTaskItemView()
+                    }
                 } //: VSTACK
             } //: ZSTACK
             .onAppear() {
