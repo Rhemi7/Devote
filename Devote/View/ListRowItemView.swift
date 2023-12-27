4cc53f5cc31f29409bb01482d24c1f03819a9c99
aderemilekun <adelefatai@yahoo.com>
2023-12-27 18:30:02 +0100

    Changed list view to a check box tile

---
+++ Devote/View/ListRowItemView.swift
@@ -0,0 +1,35 @@
+//
+//  ListRowItemView.swift
+//  Devote
+//
+//  Created by Aderemilekun Adele on 27/12/2023.
+//
+
+import SwiftUI
+
+struct ListRowItemView: View {
+    @Environment(\.managedObjectContext) var viewContext
+    @ObservedObject var item: Item
+    
+    var body: some View {
+        Toggle(isOn: $item.completion) {
+            Text(item.task ?? "")
+                .font(.system(.title2, design: .rounded))
+                .fontWeight(.heavy)
+                .foregroundColor(item.completion ? Color.pink : Color.primary)
+                .padding(.vertical, 12)
+                .animation(.default)
+        } //: TOGGLE
+        .onReceive(item.objectWillChange, perform: { _ in
+            if self.viewContext.hasChanges {
+                try? self.viewContext.save()
+            }
+        })
+    }
+}
+
+//struct ListRowItemView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ListRowItemView()
+//    }
+//}
