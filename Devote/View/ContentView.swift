021840636489f3063cbb15bbe366c064ca9e5557
aderemilekun <adelefatai@yahoo.com>
2023-12-27 18:06:05 +0100

    Implemented dark and light mode

--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     
     // MARK: - PROPERTY
     
+    @AppStorage("isDarkMode") private var isDarkMode: Bool = false
     @State var task: String = ""
     @State private var showNewTaskItem: Bool = false
    
@@ -75,9 +76,9 @@ struct ContentView: View {
                         // APPEARANCE BUTTON
                         Button(action: {
                             // TOGGLE APPEARANCE
-                            
+                            isDarkMode.toggle()
                         }, label: {
-                            Image(systemName: "moon.circle")
+                            Image(systemName: isDarkMode ? "moon.circle.fill" : "moon.circle")
                                 .resizable()
                                 .frame(width: 24, height: 24)
                                 .font(.system(.title, design: .rounded))
