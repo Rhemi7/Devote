f093486474e1c5f1071e7741a469394758b24c82
aderemilekun <adelefatai@yahoo.com>
2024-01-17 15:27:43 +0100

    Added iOS widget for app

---
+++ DevoteWidget/DevoteWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DevoteWidgetBundle.swift
+//  DevoteWidget
+//
+//  Created by Aderemilekun Adele on 17/01/2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DevoteWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DevoteWidget()
+    }
+}
